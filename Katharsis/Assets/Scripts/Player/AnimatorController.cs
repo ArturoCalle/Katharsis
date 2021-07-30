b834cca3db165f29593b00b5a96b63688532b88f
Pablo Veintemilla Ramírez <pbr.9816@gmail.com>
2021-07-29 21:21:02 -0500

    update
    refactor a las animaciones, purebas sobre animacion idle

--- Katharsis/Assets/Scripts/Player/AnimatorController.cs
+++ Katharsis/Assets/Scripts/Player/AnimatorController.cs
@@ -7,6 +7,9 @@ public class AnimatorController : MonoBehaviour
     public static AnimatorController instance;
     public GameObject trompi;
     private Animator animator;
+    private float idle = 0;
+    private float time = 0.0f;
+    private float interpolationPeriod = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +21,14 @@ public class AnimatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        time += Time.deltaTime;
 
+        if (time >= interpolationPeriod)
+        {
+            time = time - interpolationPeriod;
+            idle = Random.Range(0f, 10f);
+        }
+    }
     public void move(Vector3 inputs, float velocityY, bool isGrounded, bool jump)
     {
         if(inputs != new Vector3(0, 0, 0))
@@ -30,6 +38,8 @@ public class AnimatorController : MonoBehaviour
         else
         {
             animator.SetFloat("walk", 0);
+            animator.SetFloat("IDLE", idle);
+
         }
         animator.SetFloat("yVelocity", velocityY);
         animator.SetBool("isGrounded", isGrounded);
