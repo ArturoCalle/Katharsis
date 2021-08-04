f4b077d44a36d9e2630de3aaf132f9dad5189712
Julian Arturo Calle <julian.calle@javeriana.edu.co>
2021-08-04 16:08:39 -0500

    agregadas notas

---
+++ Katharsis/Assets/Scripts/Trigger Objects/Recolectable.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Recolectable : MonoBehaviour
+{
+    public GameObject aviso;
+    public GameObject nota;
+
+    bool recolectado = false;
+    bool inCollectRange = false;
+    // Start is called before the first frame update
+    public void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+           aviso.SetActive(true);
+           inCollectRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider col)
+    {
+        aviso.SetActive(false);
+        inCollectRange = false;
+    }
+
+    public void mostrarNota()
+    {
+        nota.SetActive(true);
+        PlayerControls.instance.freeze(true);
+        aviso.GetComponentInChildren<Text>().text = "(Z) aceptar";
+    }
+   
+    private void Update()
+    {
+        if(recolectado)
+        {
+            gameObject.SetActive(false);
+        }
+        if(inCollectRange)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                mostrarNota();
+            }
+        }
+        if (aviso.activeInHierarchy)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                aviso.SetActive(false);
+                nota.SetActive(false);
+                PlayerControls.instance.freeze(false);
+                recolectado = true;
+
+            }
+        }
+    }
+    void Start()
+    {
+        
+    }
+}
