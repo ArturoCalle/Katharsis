b975b82b0f2cbf7e475b82592ded2aed88e6328d
Pablo Veintemilla Ramírez <pbr.9816@gmail.com>
2021-09-02 19:12:08 -0500

    update

---
+++ Katharsis/Assets/Scripts/Trigger Objects/CheckPointController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    public List<CheckpointSingle> checkpoints;
+    public static CheckPointController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
+    {
+        SceneController.instance.lastCheckpoint = checkpointSingle;
+        SceneController.instance.GuardarPartida();
+    }
+}
