0299fd2b7c733115115e57f77c1c7513356642aa
Pablo Veintemilla Ramírez <pbr.9816@gmail.com>
2021-10-04 15:18:16 -0500

    sala

--- Katharsis/Assets/Scripts/Distimia/DistimiaAI.cs
+++ Katharsis/Assets/Scripts/Distimia/DistimiaAI.cs
@@ -19,13 +19,14 @@ namespace UnityStandardAssets.Assets.ThirdPerson
         public bool isAlive;
         private bool mirarTrompi;
 
-        public float radioBusqueda = 50f;
-        
+        public float radioBusqueda = 20f;
+        public float golpeBusqueda = 10f;
+
         public enum State
         {
             pasear,
             buscarTrompi,
-            mirarTrompiMientrasPasea
+            golpearTrompi
         }
         public State state;
 
@@ -49,7 +50,6 @@ namespace UnityStandardAssets.Assets.ThirdPerson
         private void Update()
         {
             float distance = Vector3.Distance(jugador.transform.position, transform.position);
-            Debug.Log(distance);
             if (SceneController.instance.getCurrentSceneName() != "Sala")
             {
                 if(distance <= radioBusqueda)
@@ -57,7 +57,14 @@ namespace UnityStandardAssets.Assets.ThirdPerson
                     //TODO chase speed
                     state = State.buscarTrompi;
                 }
-            }else
+                if (distance <= radioGolpe)
+                {
+                    //TODO chase speed
+                    state = State.golpearTrompi;
+                }
+
+            }
+            else
             {
                 mirarTrompi = true;
             }
@@ -73,6 +80,9 @@ namespace UnityStandardAssets.Assets.ThirdPerson
                         break;
                     case State.buscarTrompi:
                         BuscarTrompi();
+                        break;
+                    case State.golpearTrompi:
+                        GolpearTrompi();
                         break;
                 }
                 yield return null;
@@ -112,6 +122,12 @@ namespace UnityStandardAssets.Assets.ThirdPerson
             chaseSpeed = character.Move(agent.desiredVelocity, false);
             RigController.instance.setRigWeightToOne(jugador.transform);
         }
+
+        void GolpearTrompi()
+        {
+            //TODO
+        }
+
     }
 }
 
