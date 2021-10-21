6ae6333d0873647d70f134f48cad3cae2be76274
cmaldonado97 <camilo_maldonado@javeriana.edu.co>
2021-10-21 11:06:26 -0500

    log

--- Katharsis/Assets/Cinemáticas/Cinematica.cs
+++ Katharsis/Assets/Cinemáticas/Cinematica.cs
@@ -11,18 +11,19 @@ public class Cinematica : MonoBehaviour
     public bool reproducido = true;
     void Start()
     {
-        
+        Debug.Log(Timeline.duration.ToString());
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Timeline.time.ToString());
         if(Timeline.state.ToString() != "Played")
         {
             reproducido = false;
             Timeline.Stop();
-            cutscenecam.SetActive(false;
+            cutscenecam.SetActive(false);
         }
     }
     public void reproducir()
