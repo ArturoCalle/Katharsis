6d7f04b36b6c9580bc108c45d71e883237ff8eb0
cmaldonado97 <camilo_maldonado@javeriana.edu.co>
2021-11-04 19:46:06 -0500

    commit

--- Katharsis/Assets/Cinemáticas/Cinematica.cs
+++ Katharsis/Assets/Cinemáticas/Cinematica.cs
@@ -3,39 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
+/**
+ * Esta clase se utiliza para controlar que las cinemáticas (cutscenes) que se reproducen dentro del juego se reproduzcan.
+ * Utiliza el componente de Unity "Cinemachine" el cual permite usar gameobjects de tipo timeline y añadir cámaras virtuales a la escena.
+*/ 
 public class Cinematica : MonoBehaviour
 {
     // Start is called before the first frame update
-    public PlayableDirector Timeline;
-    public GameObject cutscenecam;
+    public PlayableDirector Timeline; //Timeline asignada desde el inspector
+    public GameObject cutscenecam; //Cámara asignada desde el inspector
     public bool reproducido = true;
-    public float duracion;
+    public float duracion; //Duración de la cinemática
     void Start()
     {
         
         
     }
 
-    // Update is called once per frame
+    /**
+     * En esta clase, el update se utiliza para controlar la duración de las cinemáticas la cual está guardada en la variable "duracion"
+     * Al reproducir una cinemática, se desactiva el PlayerControls para evitar que Trompi se pueda mover, al terminar la cinemática se vuelve a activar
+     */ 
     void Update()
     {
         duracion -= Time.deltaTime;
         if (duracion <= 0)
         {
             Timeline.Stop();
-            cutscenecam.SetActive(false);
-            PlayerControls.instance.enabled = true;
+            cutscenecam.SetActive(false); //Desactivar cámara
+            PlayerControls.instance.enabled = true; //Activar PlayerControls
             gameObject.SetActive(false);
 
 
         }        
-        if(Timeline.state.ToString() != "Played")
+        if(Timeline.state.ToString() != "Played") //
         {
             reproducido = false;
             Timeline.Stop();
             cutscenecam.SetActive(false);
         }
     }
+    /**
+     * Esta función reproduce la cinemática una vez es llamada. De igual manera, desactiva el PlayerControls para evitar que trompi
+     * se pueda mover.
+     */ 
     public void reproducir()
     {
         cutscenecam.SetActive(true);
--- Katharsis/Assets/Scripts/Trigger Objects/CheckpointSingle.cs
+++ Katharsis/Assets/Scripts/Trigger Objects/CheckpointSingle.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ * Esta clase se encarga de detectar cuando el jugador pasa por un checkpoint que está ubicado dentro de la escena. Al hacer esto
+ * la función "PlayerThroughCheckpoint" de la clase CheckPointController es llamada.
+ */
 public class CheckpointSingle : MonoBehaviour
 {
+    /**
+     * Esta función detecta si lo que tocó el objeto tiene el tag "Player" para saber si es el jugador. Luego de esto, se registra en
+     * el CheckPointController
+     */
     private void OnTriggerEnter(Collider other)
     {
         if(this.gameObject.name != "ChechPointStart")
